205b2da542ff2aeb4abbf5b556e61a1e4ee82158
asimakath <asim.ma@gmail.com>
2018-02-03 23:10:22 -0600

    init on veutifyjs/pwa, added firebase, vuefire 2.0.0-alpha.6, moment, home page that lists the biryaniDays collection

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import VueFire from 'vuefire'
+import moment from 'moment'
+
+Vue.use(VueFire)
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+Vue.filter('formatDate', function (value) {
+  if (value) {
+    return moment(String(value)).format('LL')
+  }
+})

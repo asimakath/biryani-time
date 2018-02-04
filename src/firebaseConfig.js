58ae4990e26292bbd2812af9f8c88ffad5f016b7
asimakath <asim.ma@gmail.com>
2018-02-03 23:10:22 -0600

    init on vuetifyjs/pwa, added firebase, vuefire 2.0.0-alpha.6, moment, home page that lists the biryaniDays collection

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: 'AIzaSyDTxZPMK9N1lqN0R-wouwi3tM_S20Sm3sw',
+  authDomain: 'its-biryani-time.firebaseapp.com',
+  databaseURL: 'https://its-biryani-time.firebaseio.com',
+  projectId: 'its-biryani-time',
+  storageBucket: 'its-biryani-time.appspot.com',
+  messagingSenderId: '852186675899'
+}
+export default { config }
